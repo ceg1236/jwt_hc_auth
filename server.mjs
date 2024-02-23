cda29a18e7dfafe7325b363f7c365dcb4646ad6c
Carl Goldberg <carlgoldberg1236@gmail.com>
2024-02-23 15:10:05 -0800

    more simpler

--- server.mjs
+++ server.mjs
@@ -6,19 +6,19 @@ import jwt from "jwt-simple";
 import { v4 as uuidv4 } from 'uuid';
 
 // Access your shared key via an environment variable
-const shared_key = process.env.SHARED_KEY;
+// const shared_key = process.env.SHARED_KEY;
 
-function generateJwt(name, email) {
-  const payload = {
-    iat: Math.floor(new Date().getTime() / 1000),
-    jti: uuidv4(),
-    name: name,
-    email: email
-  };
-  console.log('paylod ===', payload);
-  // encode
-  return jwt.encode(payload, shared_key);
-}
+// function generateJwt(name, email) {
+//   const payload = {
+//     iat: Math.floor(new Date().getTime() / 1000),
+//     jti: uuidv4(),
+//     name: name,
+//     email: email
+//   };
+//   console.log('paylod ===', payload);
+//   // encode
+//   return jwt.encode(payload, shared_key);
+// }
 
 const app = express();
 const port = 3000;
